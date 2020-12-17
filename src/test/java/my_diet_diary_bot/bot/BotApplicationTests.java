512ef16553d63cc382b3d4e56d3bd4285511627e
DenisViskov <DenisViskov16@gmail.com>
2020-12-17 09:32:35 +0300

    Initial commit

---
+++ src/test/java/my_diet_diary_bot/bot/BotApplicationTests.java
@@ -0,0 +1,13 @@
+package my_diet_diary_bot.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
