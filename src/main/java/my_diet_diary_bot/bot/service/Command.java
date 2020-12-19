1ac746b2653bcccbc8f56a87a28e5f26634d60b1
DenisViskov <denisviskov16@gmail.com>
2020-12-19 16:17:48 +0300

    update

--- src/main/java/my_diet_diary_bot/bot/service/Command.java
+++ src/main/java/my_diet_diary_bot/bot/service/Command.java
@@ -1,25 +1,11 @@
 package my_diet_diary_bot.bot.service;
 
 /**
- * Enum
- *
+ * Интерфейс реализующий способность
  * @author Денис Висков
  * @version 1.0
- * @since 17.12.2020
+ * @since 19.12.2020
  */
-public enum Command {
-    ADD_FOOD("/ADD_FOOD"),
-    REPLACE_FOOD("/REPLACE_FOOD"),
-    DELETE_FOOD("/DELETE_FOOD"),
-    ;
-
-    private final String option;
-
-    Command(String option) {
-        this.option = option;
-    }
-
-    public String getOption() {
-        return option;
-    }
+public interface Command<T,V> {
+  T executeCommand(V message);
 }
